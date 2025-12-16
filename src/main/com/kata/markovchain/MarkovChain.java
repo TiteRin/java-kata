b46099ad484e904b872b5f0b3213d274301862ab
Marine Lancien <marine.lancien@protonmail.com>
2025-12-16 09:30:29 +0100

    feat(markov-chain): implement text generation logic
    - Add logic to generate text using Markov chain states and transition probabilities.
    - Ensure generated text respects the specified length.
    - Include fallback mechanism for terminating on empty transitions.
    - Add comprehensive test to verify generated words originate from original text.

--- src/main/com/kata/markovchain/MarkovChain.java
+++ src/main/com/kata/markovchain/MarkovChain.java
@@ -1,5 +1,9 @@
 package com.kata.markovchain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 class MarkovChain {
 
     private final TextAnalyzer textAnalyzer;
@@ -11,7 +15,32 @@ class MarkovChain {
     }
 
     public String generateText() {
-        String[] result = new String[this.length];
-        return String.join(" ", result);
+
+        Map<String, State> states = this.textAnalyzer.getStates();
+
+        List<String> words = new ArrayList<>();
+
+        State nextStep = states.entrySet().stream().findAny().get().getValue();
+
+        do {
+            String currentWord = nextStep.getName();
+            State currentStep = states.get(currentWord);
+            words.add(currentWord);
+
+            if (currentStep.getTransitions().isEmpty()) break;
+
+            double random = Math.random();
+            double cumulativeProbability = 0.0;
+
+            for(Map.Entry<State, Double> entry: currentStep.getTransitions().entrySet()) {
+                cumulativeProbability += entry.getValue();
+                if (random <= cumulativeProbability) {
+                    nextStep = entry.getKey();
+                }
+            }
+
+        } while (words.size() < this.length);
+
+        return String.join(" ", words);
     }
 }
